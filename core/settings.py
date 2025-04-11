60f6a736d005e8d16fb76f7db79419512c8e9f62
Emmanuel Obioma <78274374+S-cloud-tech@users.noreply.github.com>
2025-04-11 20:59:01 +0100

    host

--- core/settings.py
+++ core/settings.py
@@ -17,7 +17,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', '127.0.0.1:8000', 'easebackend.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['https://easebackend.onrender.com', 'http://127.0.0.1:8000']
 
 # Application definition
 
