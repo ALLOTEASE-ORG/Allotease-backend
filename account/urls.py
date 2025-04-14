63692c49c23b9e1814842231f20ef187bf2bcc9c
Emmanuel Obioma <78274374+S-cloud-tech@users.noreply.github.com>
2025-04-14 19:15:17 +0100

    .

--- account/urls.py
+++ account/urls.py
@@ -2,6 +2,6 @@ from rest_framework.routers import DefaultRouter
 from .views import AuthViewSet
 
 router = DefaultRouter()
-router.register(r'auth', AuthViewSet, basename='auth')
+router.register(r'', AuthViewSet, basename='auth')
 
 urlpatterns = router.urls
